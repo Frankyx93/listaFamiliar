6591245ecceaa54479a863caf043a712cd6fbf54
Fran Marcos <franmarcos93@gmail.com>
2017-11-22 12:30:06 +0100

    Añadiento metodo paso1

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,25 @@
 'use strict';
 
 module.exports = function(Usuario) {
+    /**
+    * acepta las solicitudes pendientes
+    * @param {object} contexto argumento necesario para recoger el token
+    * @param {Function(Error, array)} callback
+    */
+
+   Usuario.prototype.aceptarSolicitud = function(contexto, callback) {
+     var miembrosLista;
+     //consigo el id del solicitante NO SE SI ES NECESARIO EL .id
+     var idSolicitante = this.id;
+     //consigo el id de la lista familiar a la que pertenezco
+     //
+     //compruebo si el solicitante tiene alguna solicitud en mi lista
+     //
+     //lo meto en mi lista
+     //
+     //borro la solicitud
+     // TODO
+     callback(null, miembrosLista);
+   };
 
 };
