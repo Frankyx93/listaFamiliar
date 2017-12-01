7e86109100bfba9225647f6546d0d88694a6b552
Fran Marcos <franmarcos93@gmail.com>
2017-12-01 09:50:17 +0100

    Agregado metodo rechazarSolicitudes

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -47,4 +47,50 @@ module.exports = function(Usuario) {
          });     
      });
    };
+        /**
+      * Elimina la solicitud de un usuario en mi lista familiar
+      * @param {boolean} contexto argumento necesario para recoger el token
+      * @param {Function(Error, array)} callback
+      */
+
+     Usuario.prototype.rechazarSolicitud = function(contexto, callback) {
+     var miembrosLista;
+     var usuarioLogueadoListaFamiliarId;
+     
+     
+     //consigo el id del solicitante
+     var solicitante = this;
+     
+     
+     //consigo el id de la lista familiar a la que pertenezco
+     var usuarioLogueadoId = contexto.req.accessToken.userId;
+     Usuario.findById(usuarioLogueadoId, function(err, usuario){
+         if(err)callback(err);
+         usuarioLogueadoListaFamiliarId = usuario.listaFamiliarId;
+
+     
+     
+        //compruebo si el solicitante tiene alguna solicitud en mi lista
+        solicitante.solicitudes.findById(usuarioLogueadoListaFamiliarId, function(err, solicitud){
+           if(err)callback(err);
+
+
+       
+
+
+       //borro la solicitud
+            solicitante.solicitudes.remove(solicitud, function(err, solicitud){
+                if(err)callback(err);
+                
+                Usuario.find({where:{listaFamiliarId:usuarioLogueadoListaFamiliarId}}, function(err, instances){
+                    if(err)callback(err);
+                    miembrosLista = instances;
+       //devuelvo el array de miembros 
+                    callback(null, miembrosLista);
+                      });
+             });
+         });     
+     });
+   };
+
 };
